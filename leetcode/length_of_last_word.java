1494f99fae3eac74373793d7cd79f14d75c27c7a
Calcifer077 <maheshnashier14@gmail.com>
2024-12-29 23:31:59 +0530

    Did some leetcode

---
+++ leetcode/length_of_last_word.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class length_of_last_word {
+    public static int lengthOfLastWord(String s) {
+
+        int pos = -1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                pos = i;
+                break;
+            }
+        }
+
+        int size = 0;
+        for (int i = pos; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                size++;
+            } else {
+                break;
+            }
+        }
+
+        return size;
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello   world    ";
+        System.out.println(lengthOfLastWord(s));
+    }
+}
