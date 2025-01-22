9ea892f00a8c604a823b239e5e13a58579e34c20
Calcifer077 <maheshnashier14@gmail.com>
2025-01-22 12:44:01 +0530

    Did some leetcode

---
+++ leetcode/find_common_characters.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class find_common_characters {
+    public static List<String> commonChars(String[] words) {
+        List<String> res = new ArrayList<>();
+
+        // This is the hashmap of the first string.
+        HashMap<Character, Integer> finalMap = new HashMap<>();
+        for (int i = 0; i < words[0].length(); i++) {
+            char ch = words[0].charAt(i);
+            finalMap.put(ch, finalMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            // Creating a hashmap for each consecutive string
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (int j = 0; j < words[i].length(); j++) {
+                char ch = words[i].charAt(j);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            }
+
+            // Checking if characters of first string is present in this string or not.
+            for (char c : finalMap.keySet()) {
+                if (map.containsKey(c)) {
+                    // If present then take minimum of both as question says to include duplicates.
+                    finalMap.put(c, Math.min(map.get(c), finalMap.get(c)));
+                } else {
+                    // We can't directly remove 'c' here, it will give
+                    // 'concurrentModificationError'.
+                    // Instead we will reduce it to 0, and while making resultant list we will only
+                    // take those whose values are more than 0.
+                    finalMap.put(c, 0);
+                }
+            }
+        }
+
+        for (char c : finalMap.keySet()) {
+            for (int i = 0; i < finalMap.get(c); i++) {
+                res.add(Character.toString(c));
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "bella", "label", "roller" };
+        System.out.println(commonChars(words));
+    }
+}
---
+++ leetcode/majority_element_II.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class majority_element_II {
+    // Brute force approach
+    /*
+     * 
+     * public static List<Integer> majorityElement(int[] nums) {
+     * List<Integer> res = new ArrayList<>();
+     * 
+     * HashMap<Integer, Integer> map = new HashMap<>();
+     * for (int i = 0; i < nums.length; i++) {
+     * if (map.containsKey(nums[i])) {
+     * map.put(nums[i], map.get(nums[i]) + 1);
+     * } else {
+     * map.put(nums[i], 1);
+     * }
+     * }
+     * 
+     * for (int i : map.keySet()) {
+     * if (map.get(i) > nums.length / 3) {
+     * res.add(i);
+     * }
+     * }
+     * 
+     * return res;
+     * }
+     */
+
+    // Solving using Boyer-moore voting alogrithm
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        int count = 0, candidate = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else {
+                if (nums[i] == candidate) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            }
+
+            if (count > nums.length / 3) {
+                res.add(nums[i]);
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 3, 2, 3 };
+        System.out.println(majorityElement(nums));
+    }
+}
